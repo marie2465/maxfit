e842e2f599501a07402a9f29e8d47b8246014bb0
Maria <marie.yangol2465@gmail.com>
2021-08-31 16:54:22 +0200

    curved navigation bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:maxfit/components/active-workouts.dart';
 import 'package:maxfit/components/workouts-list.dart';
 import 'package:maxfit/domain/workout.dart';
 import 'package:maxfit/pages/auth.dart';
 import 'package:maxfit/services/auth.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int sectionIndex = 0;
 
   @override
@@ -15,7 +21,8 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
         appBar: AppBar(
-          title: Text('Maxfit'),
+          title: Text('Maxfit // ' +
+              (sectionIndex == 0 ? 'Active Workouts' : 'FindWorkouts')),
           leading: Icon(Icons.fitness_center),
           actions: <Widget>[
             FlatButton.icon(
@@ -30,22 +37,19 @@ class HomePage extends StatelessWidget {
             )
           ],
         ),
-        body: WorkoutsList(),
+        body: sectionIndex == 0 ? ActiveWorkouts() : WorkoutsList(),
         bottomNavigationBar: BottomNavigationBar(
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(Icons.fitness_center),
-              title: Text('My Workouts')
-            ),
+                icon: Icon(Icons.fitness_center), title: Text('My Workouts')),
             BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                title: Text('Find Workouts')
-            ),
+                icon: Icon(Icons.search), title: Text('Find Workouts')),
           ],
-          currentIndex: 0,
+          currentIndex: sectionIndex,
           backgroundColor: Colors.white30,
-          onTap: (int index){
-            sectionIndex = index;
+          selectedItemColor: Colors.white,
+          onTap: (int index) {
+            setState(() => sectionIndex = index);
           },
         ),
       ),
